6d967686ec7cb63cf451a996953a5ad3ec0e5180
Paul <paul@pattersonmultimedia.com>
2024-04-09 18:39:20 -0500

    new changes

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,22 +8,22 @@ namespace HipHopPizzaWings.Models
 		public int Id { get; set; }
 		[Required]
 		public bool IsPhone { get; set; }
-		public string OrderType { get; set; }
-		public string CustomerName { get; set; }
-		public string Email { get; set; }
-		public string PhoneNumber { get; set; }
-		public int OrderId { get; set; }
+		public string? OrderType { get; set; }
+		public string? CustomerName { get; set; }
+		public string? Email { get; set; }
+		public string? PhoneNumber { get; set; }
+		public int? OrderId { get; set; }
 		public decimal? TotalPrice
 		{
 			get; set;
 		}
-		public string PaymentType { get; set; }
-		public string OrderStatus { get; set; }
-		public decimal TipAmount { get; set; }
+		public string? PaymentType { get; set; }
+		public string? OrderStatus { get; set; }
+		public decimal? TipAmount { get; set; }
 		public DateTime DateCreated { get; set; }
 		public int CashierId { get; set; }
 
-		public ICollection<OrderItem> OrderItems { get; set; }
+		public ICollection<OrderItem>? OrderItems { get; set; }
 	}
 }
 
